bbdefca96b3ff41bf25e427708ebe0ab6e72c632
JC Tula <jtula_esfe@outlook.es>
2023-10-16 17:06:57 -0600

    Se corrigió problema de borrado y modificación de usuarios

---
+++ FrontendAdministracion/Models/UsuarioEdit.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontendAdministracion.Models
+{
+    public class UsuarioEdit
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El Rol es obligatorio")]
+        public int IdRol { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido es obligatorio")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El Login es obligatorio")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public int Estatus { get; set; }
+    }
+}
